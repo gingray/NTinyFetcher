a91d1c9dc6cba31a956002c9a191bf38384e83a6
gingray <gingray.dev@gmail.com>
2015-01-18 04:04:06 +0300

    Initial

---
+++ NTinyFetcher/FetcherBase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NTinyFetcher
+{
+    public abstract class FetcherBase<TClient> where TClient : class, IWebClient
+    {
+        private readonly TClient _webClient;
+
+        protected FetcherBase(TClient webClient)
+        {
+            _webClient = webClient;
+        }
+
+        public void Perform()
+        {
+            Configure(_webClient);
+            while (true)
+            {
+                var url = GetUrl();
+                if(url == null)
+                    break;
+                try
+                {
+                    MakeAction(url, _webClient);
+                }
+                catch (Exception ex)
+                {
+                    ExceptionRecieve(ex);
+                }
+            }
+        }
+
+        protected abstract void MakeAction(string url, TClient webClient);
+        protected abstract void Configure(TClient webClient);
+        protected abstract string GetUrl();
+        protected abstract void ExceptionRecieve(Exception ex);
+    }
+}
---
+++ NTinyFetcher/IWebClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NTinyFetcher
+{
+    public interface IWebClient
+    {
+        string Get(string url);
+    }
+}
